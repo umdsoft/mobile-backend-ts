298f47a183f323aef188d86da10baefb44dc8fb0
Umidbek Jumaniyozov <jumaniyozovu@gmail.com>
2021-02-24 08:26:11 +0500

    edit bugs

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 import { createServer } from 'http';
 import bodyParser from 'body-parser'
-
+import createRoutes from "./routes";
 const app = express();
 const http = createServer( app );
 import './database/connection';
 app.use(bodyParser.json());
-
+createRoutes(app);
 
 // bu joyda kod joylashadi
 
