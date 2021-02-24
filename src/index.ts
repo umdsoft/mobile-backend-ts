a5aff2a04e0d106aea4a3648eee803afff9a38ad
Umidbek Jumaniyozov <jumaniyozovu@gmail.com>
2021-02-24 08:27:17 +0500

    asd

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 import express from 'express';
 import { createServer } from 'http';
-import bodyParser from 'body-parser'
+import bodyParser from 'body-parser';
+import path from "path";
 import createRoutes from "./routes";
 const app = express();
 const http = createServer( app );
 import './database/connection';
 app.use(bodyParser.json());
 createRoutes(app);
+app.use(express.urlencoded({ extended: false }));
 
+app.use(express.static(path.join(__dirname, 'public')));
 // bu joyda kod joylashadi
 
 const PORT = process.env.PORT || 3003;
